254fc9c41944f8ce7405200ff3125fd7d38ad21d
Ashish <ashish74624@gmail.com>
2024-03-16 01:39:45 +0530

    Publisher page complete

--- client/app/publisher/page.tsx
+++ client/app/publisher/page.tsx
@@ -4,6 +4,7 @@ import {SubmitHandler, useForm} from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod";
 import {z} from 'zod'
 import toast, { Toaster } from 'react-hot-toast';
+import { BackgroundGradient } from "@/Components/ui/background-gradient";
 
 const backend = process.env.BACKEND;
 
@@ -38,23 +39,20 @@ export default function Publisher() {
     }
 
   return (
-    <section className='h-screen w-screen p-6 bg-[#F5F2EE] overflow-hidden '>
-      <div className='bg-[#F5F2EE] shadow-2xl h-full w-full border-[#4d2d18] rounded-lg border-4 overflow-hidden relative flex justify-center items-center'>
-            <div className='bg-[#4d2d18] absolute  w-80 h-80 rounded-full bottom-[75vh] right-[85vw]  hover:w-96 hover:h-96 transition-all'></div>
-            <div className='bg-[#4d2d18] absolute  w-80 h-80 rounded-full top-[75vh] left-[85vw]  hover:w-96 hover:h-96 transition-all'></div>
-            <div className={` w-[2px] h-[500px] bg-[#4d2d18] absolute top-0 right-0 rotate-45 origin-top `}></div>
-            <div className=" w-[2px] h-[500px] bg-[#4d2d18] absolute top-[50vh] rotate-45 left-[300px] origin-top"></div>
-            <div className=' absolute  w-20 h-20 rounded-full blur-md animate-bounce bottom-1 right-4'></div>
-            <form onSubmit={handleSubmit(onSubmit)} className=' bg-white w-80 rounded-lg flex flex-col items-center p-6 h-max pb-7 shadow-lg' >
-
-                <label className='text-[#4d2d18] text-2xl ' htmlFor="name">Enter Publisher Name</label>
+    <section className='h-screen w-screen p-6 bg-neutral-900 overflow-x-hidden flex space-y-10 flex-col items-center overflow-y-scroll '>
+        <h1 className='text-5xl md:text-6xl text-white'>Add Publisher</h1>
+        <BackgroundGradient className="rounded-[22px] max-w-sm p-10  bg-zinc-900">
+            <form
+            onSubmit={handleSubmit(onSubmit)} 
+            className=' w-full rounded-lg flex flex-col items-center h-full '>
+                <label className='text-white text-2xl ' htmlFor="name">Enter Publisher Name</label>
                 <input {...register('name')} type="text" name='name' className='w-full bg-gray-50 border border-black px-2 rounded-lg mt-4 py-2' />
-                <button disabled={isSubmitting} type='submit' className='mt-4 w-full bg-[#4d2d18] text-white py-2 rounded-lg'>
+                <button disabled={isSubmitting} type='submit' className='mt-4 w-full bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)] text-white py-2 rounded-lg'>
                     {isSubmitting ? "Loading...":"Add Publisher"} 
                 </button>
             </form> 
-        </div>
+        </BackgroundGradient>
         <Toaster />
-      </section>
+    </section>
   )
 }
