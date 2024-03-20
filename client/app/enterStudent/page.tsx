9ea7ff439482cc6fcc340b21584be1fabf799b43
Ashish <ashish74624@gmail.com>
2024-03-21 04:30:47 +0530

    student page redesign

--- client/app/enterStudent/page.tsx
+++ client/app/enterStudent/page.tsx
@@ -4,7 +4,8 @@ import {SubmitHandler, useForm} from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod';
 import {z} from 'zod'
 import toast, { Toaster } from 'react-hot-toast';
-import Design from '@/Components/Design';
+import { BackgroundGradient } from "@/Components/ui/background-gradient";
+
 
 const schema = z.object({
     Student_ID:z.number(),
@@ -45,11 +46,11 @@ export default function EnterStudent() {
     }
 
   return (
-    <section className='h-screen w-screen p-6 bg-[#F5F2EE] overflow-hidden '>
-      <div className='bg-[#F5F2EE] shadow-2xl h-full w-full border-[#4d2d18] rounded-lg border-4 overflow-hidden relative flex justify-center items-center'>
-            <Design/>
-            <form onSubmit={handleSubmit(onSubmit)} className='bg-white relative z-10 h-max rounded-lg shadow-lg w-80 flex flex-col pt-2 px-6 pb-6 '>
-                <h1 className='w-max mx-auto mb-2 text-2xl text-[#4d2d18] ' >Enter Student Details</h1>
+    <section className='h-screen w-screen p-6 bg-neutral-900 overflow-x-hidden flex space-y-10 flex-col items-center overflow-y-scroll'>
+        <h1 className='text-5xl md:text-6xl text-white'>Add Student</h1>
+        <BackgroundGradient className="rounded-[22px] max-w-sm  bg-zinc-900">
+            <form onSubmit={handleSubmit(onSubmit)}  className=' w-full rounded-lg flex flex-col p-4 h-full text-white '>
+                <h1 className='w-max mx-auto mb-2 text-2xl ' >Enter Student Details</h1>
                 <label className='mt-2' htmlFor="Student_ID" >Student Id</label>
                 <input type="number" {...register('Student_ID',{ valueAsNumber: true })} className='bg-gray-100 rounded-md border py-1 px-2 '  />
                 <label className='mt-2' htmlFor="Student_Name">Student Name</label>
@@ -61,11 +62,11 @@ export default function EnterStudent() {
                 <input type="number" {...register('Semester',{ valueAsNumber: true })} className='bg-gray-100 rounded-md border py-1 px-2 '  />
                 <label className='mt-2' htmlFor="Student_Phone" >Student Phone</label>
                 <input type="number" {...register('Student_Phone',{ valueAsNumber: true })} className='bg-gray-100 rounded-md border py-1 px-2 '  />
-                <button disabled={isSubmitting} type='submit' className='mt-4 w-full bg-[#4d2d18] text-white py-2 rounded-lg'>
+                <button disabled={isSubmitting} type='submit' className='mt-4 w-full bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)] text-white py-2 rounded-lg'>
                     {isSubmitting ? "Loading...":"Register Student"} 
                 </button>
             </form>
-        </div>
+        </BackgroundGradient>
         <Toaster />
       </section>
   )
